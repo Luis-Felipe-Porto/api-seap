10e0efab9f75a3c23b9d75ab43bb7b7655962243
unknown <LuisPorto>
2021-10-19 20:42:21 -0300

    realizando monitoramento com actuator na api

--- src/main/java/gov/ma/apiseap/config/SwaggerConfig.java
+++ src/main/java/gov/ma/apiseap/config/SwaggerConfig.java
@@ -1,24 +1,13 @@
 package gov.ma.apiseap.config;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 
